bf7c045514d19edaef74d356cd3226b4b481328c
Jimmy Wärting <jimmy@warting.se>
2021-02-16 18:23:10 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,31 @@
-globalThis.addEventListener('push', async evt => {
+/// <reference lib="es2020" />
+/// <reference lib="webworker" />
+
+/**
+ * @type {ServiceWorkerGlobalScope}
+ */// @ts-ignore
+const sw = globalThis
+
+sw.addEventListener('install', () => {
+  sw.skipWaiting()
+})
+
+sw.addEventListener('activate', event => {
+  event.waitUntil(sw.clients.claim())
+})
+
+sw.addEventListener('push', async evt => {
   // Read the push message that was sent
   const payload = evt.data?.json()
 
   // Broadcast the message to all tabs
-  globalThis.clients.matchAll().then(clients => {
+  sw.clients.matchAll().then(clients => {
     clients.forEach(client => client.postMessage(payload))
   })
+})
+
+sw.addEventListener('message', evt => {
+  if (evt.data === 'claimMe') {
+    sw.clients.claim()
+  }
 })
